3256c23127b5589b6c70ef1f995742feb9c55e52
Semias <Stefan.gall08@gmail.com>
2023-01-23 00:13:20 +0100

    renderer ScrollTop

--- frontend/src/app/components/scroll-top/scroll-top.component.ts
+++ frontend/src/app/components/scroll-top/scroll-top.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-top',
   templateUrl: './scroll-top.component.html',
   styleUrls: ['./scroll-top.component.scss'],
 })
-export class ScrollTopComponent {}
+export class ScrollTopComponent {
+  ngOninit() {}
+
+  scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+
+  @ViewChild("scroll-top-button") scrolltop: any;
+
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener("window:scroll")
+  adjustHeights() {
+    console.log("Into scroll");
+    if (window.pageYOffset > 200) {
+      this.scrolltop.class.remove("invisible");
+    } else {
+    }
+  }
+}
