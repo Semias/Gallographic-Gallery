7b84ab5f324b9fe28c1b89fae7b6fed35af5b28b
Semias <Stefan.gall08@gmail.com>
2023-02-24 15:58:49 +0100

    remake backend and start defining color-scheme variables

--- backend/src/admin/app.js
+++ backend/src/admin/app.js
@@ -1,31 +1,35 @@
+import favicon from './extensions/favicon.ico';
+import logo from './extensions/logo.svg';
+
+
 const config = {
   locales: [
-    'ar',
-    'fr',
-    'cs',
+    // 'ar',
+    // 'fr',
+    // 'cs',
     'de',
-    'dk',
-    'es',
-    'he',
-    'id',
-    'it',
-    'ja',
-    'ko',
-    'ms',
-    'nl',
-    'no',
-    'pl',
-    'pt-BR',
-    'pt',
-    'ru',
-    'sk',
-    'sv',
-    'th',
-    'tr',
-    'uk',
-    'vi',
-    'zh-Hans',
-    'zh',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
   ],
 };
 
@@ -34,6 +38,38 @@ const bootstrap = (app) => {
 };
 
 export default {
-  config,
+  config: {
+    auth: {
+      logo: logo,
+    },
+    head: {
+      favicon: favicon,
+    },
+    locales: ['de'],
+    // theme: {
+    //   // overwrite light theme properties
+    //   light: {
+    //     colors: {
+    //       primary100: '#f6ecfc',
+    //       primary200: '#e0c1f4',
+    //       primary500: '#ac73e6',
+    //       primary600: '#9736e8',
+    //       primary700: '#8312d1',
+    //       danger700: '#b72b1a'
+    //     },
+    //   },
+    //   // overwrite dark theme properties
+    //   dark: {
+    //     colors: {
+    //       primary100: '#f6ecfc',
+    //       primary200: '#e0c1f4',
+    //       primary500: '#ac73e6',
+    //       primary600: '#9736e8',
+    //       primary700: '#8312d1',
+    //       danger700: '#b72b1a'
+    //     },
+    //   }
+    // },
+  },
   bootstrap,
 };
