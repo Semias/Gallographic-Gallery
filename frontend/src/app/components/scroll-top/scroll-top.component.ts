6416b86a4200f6590d931332b8ba854d78d2f20e
Semias <Stefan.gall08@gmail.com>
2023-02-24 19:28:02 +0100

    Theme fixes and scroll-top extension

--- frontend/src/app/components/scroll-top/scroll-top.component.ts
+++ frontend/src/app/components/scroll-top/scroll-top.component.ts
@@ -1,28 +1,33 @@
-import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-scroll-top',
   templateUrl: './scroll-top.component.html',
   styleUrls: ['./scroll-top.component.scss'],
 })
-export class ScrollTopComponent {
+export class ScrollTopComponent implements AfterViewInit {
+  @ViewChild('scroller') scroller!: ElementRef;
+
   ngOninit() {}
+
+  ngAfterViewInit() {}
+
+  @HostListener('window:scroll')
+  adjustHeights() {
+    if (window.pageYOffset > 150) {
+      this.scroller.nativeElement.classList.remove('invisible');
+    } else {
+      this.scroller.nativeElement.classList.add('invisible');
+    }
+  }
 
   scrollTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-
-
-  @ViewChild("scroll-top-button") scrolltop: any;
-
-  constructor(private renderer: Renderer2) {}
-
-  @HostListener("window:scroll")
-  adjustHeights() {
-    console.log("Into scroll");
-    if (window.pageYOffset > 200) {
-      this.scrolltop.class.remove("invisible");
-    } else {
-    }
-  }
 }
