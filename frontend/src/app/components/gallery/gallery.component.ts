3b9eccd07315c906bbbb322031074cfe59decb63
Semias <Stefan.gall08@gmail.com>
2023-03-02 21:16:02 +0100

    gallery Modal and sliderImage testdata

--- frontend/src/app/components/gallery/gallery.component.ts
+++ frontend/src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required modules
+import SwiperCore, {
+  SwiperOptions,
+  Pagination,
+  Navigation,
+  // Autoplay,
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
 @Component({
   selector: 'app-gallery',
@@ -6,39 +18,104 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent {
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+    loop: true,
+  };
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
+
   albums = [
     {
       thumbnailSrc:
         '../../assets/images/gallery/thumbnails/aalen-thumbnail.jpg',
       thumbnailDescription: 'test1',
       thumbnailAlt: 'test1',
-      albumSliderImages: [
-        '../../assets/images/gallery/thumbnails/aalen-thumbnail.jpg',
-        '../../assets/images/gallery/thumbnails/aalen-thumbnail.jpg',
+      SliderImages: [
+        {
+          src: '../../assets/images/gallery/project/aalen/aalen01.jpg',
+          alt: 'aalen01',
+        },
+        {
+          src: '../../assets/images/gallery/project/aalen/aalen02.jpg',
+          alt: 'aalen02',
+        },
+        {
+          src: '../../assets/images/gallery/project/aalen/aalen03.jpg',
+          alt: 'aalen03',
+        },
       ],
     },
-  ];
-
-  thumbnails = [
     {
-      src: '../../assets/images/gallery/thumbnails/aalen-thumbnail.jpg',
-      alt: 'Header picture number 1',
-      description: 'Barcelona 2019',
+      thumbnailSrc:
+        '../../assets/images/gallery/thumbnails/barcelona-thumbnail.jpg',
+      thumbnailDescription: 'Barcelona 2019',
+      thumbnailAlt: 'test1',
+      SliderImages: [
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim01.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim02.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim03.jpg',
+          alt: 'testalt',
+        },
+      ],
     },
     {
-      src: '../../assets/images/gallery/thumbnails/barcelona-thumbnail.jpg',
-      alt: 'Header picture number 2',
-      description: 'Barcelona 2019',
+      thumbnailSrc: '../../assets/images/gallery/thumbnails/gti-thumbnail.jpg',
+      thumbnailDescription: 'test1',
+      thumbnailAlt: 'test1',
+      SliderImages: [
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim01.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim02.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim03.jpg',
+          alt: 'testalt',
+        },
+      ],
     },
     {
-      src: '../../assets/images/gallery/thumbnails/mannheim-thumbnail.jpg',
-      alt: 'Header picture number 3',
-      description: 'Barcelona 2019',
-    },
-    {
-      src: '../../assets/images/gallery/thumbnails/gti-thumbnail.jpg',
-      alt: 'Header picture number 3',
-      description: 'Barcelona 2019',
+      thumbnailSrc:
+        '../../assets/images/gallery/thumbnails/mannheim-thumbnail.jpg',
+      thumbnailDescription: 'test1',
+      thumbnailAlt: 'test1',
+      SliderImages: [
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim01.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim02.jpg',
+          alt: 'testalt',
+        },
+        {
+          src: '../../assets/images/gallery/project/mannheim/mannheim03.jpg',
+          alt: 'testalt',
+        },
+      ],
     },
   ];
 }
