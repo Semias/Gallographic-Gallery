bf8bbab11c02b2977a1e21b2b5fd4de5beddcf58
Semias <Stefan.gall08@gmail.com>
2023-04-08 00:26:03 +0200

    clean another round

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,32 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClient) {}
-  // albums = [];
-  // error: any = null;
-
-  getData() {
-    return this.http.get('https://api.github.com/users/semias/repos').pipe(
-      catchError((error) => {
-        console.log(error);
-        return throwError('Something went wrong on API');
-      })
-    );
-  }
-
-  // async ngOnInit() {
-  //   try {
-  //     const response = await axios.get(
-  //       '​http://localhost:1337/api/albums?populate=*'
-  //     );
-  //     this.albums = response.data;
-  //   } catch (error) {
-  //     this.error = error;
-  //   }
-  // }
+  constructor() {}
 }
